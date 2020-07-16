e67d1183b9ec734eb6a0c96d1b17644bcb445f9f
Gabrielsant1 <Gabrielrezin001@gmail.com>
2020-07-16 20:55:57 -0300

    Commit Primeira Parte do Projeto

---
+++ js/function-menu.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+
+    // Abrir a página list-disciplina dentro do index.html
+
+    $('#conteudo').load('../src/disciplinas/visao/list-disciplinas.html');
+})
---
+++ src/conexao/conn.php
@@ -0,0 +1,20 @@
+<!-- conn é a abreviatura de connection -->
+<?php
+
+    // Demonstre todos os erros e alertas existentes quanto ao, código e interação com o B.D.
+        //Os dois comandos a seguir, tem por finalidade a forçar uma configuração no servidor APACHE
+        ini_set('display_erros', true);
+        error_reporting(E_ALL);
+
+        // Criação de variáveis pra acesso ao banco de dados
+        $hostname = "localhost";
+        $username = "root";
+        $database = "minhasnotas";
+        $password = "";
+        
+
+        if($conecta = mysqli_connect($hostname, $username, $password, $database)){
+            // echo "Conexão com o Banco de Dados " .$database. " efetuado com sucesso!";
+        }else{
+            echo "Erro: " .mysqli_connect_error();
+        }
